bed6eda1bdc222feccb1d682bdd203d0a9170bc2
Thiago Sales <thiagomsales@hotmail.com>
2021-01-24 23:42:05 -0300

    Another one

--- server/config/connection.ts
+++ server/config/connection.ts
@@ -1,11 +1,15 @@
 
 import knex from "knex";
-import * as dotenv from "dotenv";
-dotenv.config({ path: process.env.NODE_ENV === "development" ? ".env.dev" : ".env" });
+if (process.env.NODE_ENV !== 'production') {
+    import('dotenv')
+        .then((dotenv) => {
+            dotenv.config({ path: process.env.NODE_ENV === "development" ? ".env.dev" : ".env" });
+        });
+}
 
 export default knex({
     client: "sqlite3",
-    connection: { 
+    connection: {
         filename: process.env.dbFilename ?? "./server/database/database.sqlite3"
     }
 });
